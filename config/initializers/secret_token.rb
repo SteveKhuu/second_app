cdcb26a5a6bd65da7db5985a04b2879b553243d5
Stephen Khuu <Stephen_Khuu@epam.com>
2012-10-19 14:15:00 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = '4688ca48b11e363cc630ea4423df4ad9fbc4ebe2c4de0426d24473d17643e91d029b7ae5df7aeb962fdb884d45563f627069f92984ad90b312a0cb61cb296747'
